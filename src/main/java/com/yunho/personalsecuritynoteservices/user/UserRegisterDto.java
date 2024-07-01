ff22972687e4813a38b74ad3c0f560638a5bc569
YunhoNam <nam100400@naver.com>
2024-07-01 15:35:10 +0900

    Add - User Test

--- src/main/java/com/yunho/personalsecuritynoteservices/user/UserRegisterDto.java
+++ src/main/java/com/yunho/personalsecuritynoteservices/user/UserRegisterDto.java
@@ -1,13 +1,11 @@
 package com.yunho.personalsecuritynoteservices.user;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class UserRegisterDto {
 
     private String username;

24c4692d1a7c0ebd321e52901b4e2d0b1a1f6fc5
YunhoNam <nam100400@naver.com>
2024-07-01 15:03:24 +0900

    Add - UserNotFoundException

---
+++ src/main/java/com/yunho/personalsecuritynoteservices/user/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.yunho.personalsecuritynoteservices.user;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+        super("유저를 찾을 수 없습니다.");
+    }
+}

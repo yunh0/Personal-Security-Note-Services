e31a2c2150bf34111b64ce426ac102097a2e61cb
YunhoNam <nam100400@naver.com>
2024-07-01 20:53:39 +0900

    Update - claims에 roles 제외

--- src/main/java/com/yunho/personalsecuritynoteservices/jwt/JwtUtils.java
+++ src/main/java/com/yunho/personalsecuritynoteservices/jwt/JwtUtils.java
@@ -23,7 +23,6 @@ public class JwtUtils {
 
     public static String createToken(User user) {
         Claims claims = Jwts.claims().setSubject(user.getUsername());
-        claims.put("roles", user.getAuthorities());
         Date now = new Date(); // 현재 시간
         Pair<String, String> key = JwtKey.getRandomKey();
 
